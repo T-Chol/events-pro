c36660fd067b0c920171f194d92cdd1428fcb58b
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-09-10 21:37:44 +0300

    Configure application routes and protected routes in Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,67 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import EventList from './components/EventList';
+import EventDetail from './components/EventDetail';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import UserProfile from './components/UserProfile';
+import BookingPage from './components/BookingPage';
+import OrderHistory from './components/OrderHistory';
+import Settings from './components/Settings';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 
-// Placeholder components
-const HomePage = () => <div>Home Page</div>;
-const AboutPage = () => <div>About Page</div>;
-const ContactPage = () => <div>Contact Page</div>;
-const NotFoundPage = () => <div>404 - Page Not Found</div>;
-
-function Routes() {
+const AppRoutes = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Navbar />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/events" element={<EventList />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route 
+          path="/profile" 
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/book" 
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/orders" 
+          element={
+            <ProtectedRoute>
+              <OrderHistory />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/settings" 
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
     </Router>
   );
-}
+};
 
-export default Routes;
+export default AppRoutes;
