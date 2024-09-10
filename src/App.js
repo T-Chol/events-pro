6b748e8805d7aeec74111563cbca5569faa11609
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-09-10 21:35:54 +0300

    Adds a modified Apps.js file with the necessary component imports and route paths functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import './App.css';
-// Import your components here
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import EventList from './components/EventList';
+import EventDetail from './components/EventDetail';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import UserProfile from './components/UserProfile';
+import BookingPage from './components/BookingPage';
+import OrderHistory from './components/OrderHistory';
+import Settings from './components/Settings';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* Add your header content here */}
-      </header>
-      <main>
-        {/* Add your main content here */}
-      </main>
-      <footer>
-        {/* Add your footer content here */}
-      </footer>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/events" element={<EventList />} />
+        <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProtectedRoute component={UserProfile} />} />
+        <Route path="/book" element={<ProtectedRoute component={BookingPage} />} />
+        <Route path="/orders" element={<ProtectedRoute component={OrderHistory} />} />
+        <Route path="/settings" element={<ProtectedRoute component={Settings} />} />
+      </Routes>
+    </Router>
   );
 }
 
