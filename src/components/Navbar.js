afec26395271cbadd56f35e6809a54a2d16a42f2
T-Chol <taban.chol@student.moringaschool.com>
2024-09-12 18:08:35 +0300

    adds drop option for login and register

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/events">Events</Link>
       <Link to="/profile">Profile</Link>
-      <Link to="/login">Login</Link>
+      <button className="loginButton" onClick={toggleDropdown}>
+        Login
+      </button>
+      {isOpen && (
+        <div>
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </div>
+      )}
     </nav>
   );
 };
